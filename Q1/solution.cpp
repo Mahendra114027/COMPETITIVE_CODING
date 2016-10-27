ab75f7e7d46f57e5082d6305d0a64c25d231145c
Himanshu4746 <himanshukumar5208@gmail.com>
2016-10-27 21:44:37 +0530

    First Question

---
+++ Q1/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n,a[1000],s,d,i,flag=0,cnt=0,ind;
+    cin>>n;
+    for(i=1;i<=n;i++){
+    	cin>>a[i];
+    }
+    cin>>s>>d;
+    ind=s;
+    while(flag==0 && cnt<n){
+    	if(ind==d){
+    		flag=1;
+    		break;
+    	}
+    	ind=a[ind];
+    	cnt++;
+    }
+    if(flag==1)
+    	cout<<"Possible";
+    else
+    	cout<<"Not_Possible";
+    return 0;
+}
